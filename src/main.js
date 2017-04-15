5a230499290ca9f4fc7b360ca763ef91b7c4f83f
Fabian Brosda <f.brosda@gmx.de>
2017-04-15 16:36:25 +0200

    Playing with map, filter and iterable stuff

---
+++ src/main.js
@@ -0,0 +1,68 @@
+
+// TODO:
+//  - foldL/foldR
+//  - currying
+//  - function composition
+
+function range( first = 0, last = Infinity, step = 1 )
+{
+    return { [Symbol.iterator]: iterate }
+
+    function* iterate( f = first, l = last, s = step ) 
+    {
+        if( f <= l ) {
+            yield f;
+            yield *iterate( f+s )
+        }
+    }
+}
+
+function map( f, iterable )
+{
+    return {
+        [Symbol.iterator]: () => {
+            const iter = iterable[Symbol.iterator]();
+            return { 
+                next: (...args) => {
+                    const v = iter.next( ...args );
+                    return v.done ? v 
+                                  : { value: f( v.value ), done: false }
+                }
+            };
+        }
+    };
+}
+
+function filter( f, iterable )
+{
+    return {
+        [Symbol.iterator]: () => {
+            const iter = iterable[Symbol.iterator]();
+            return { next: gen_next( iter )};
+        }
+    }
+
+    function gen_next( iter )
+    {
+        return next;
+
+        function next(...args) {
+            const v = iter.next( ...args );
+            return v.done 
+                    ? v : f( v.value ) 
+                    ? v : next( ...args )
+            ;
+        }
+    }
+}
+
+function foldL() 
+{
+    throw "Implement me!";
+}
+
+function foldR() 
+{
+    throw "Implement me!";
+}
+
